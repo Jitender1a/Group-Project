5d16e733958573da1ad3b6724f2e41599d5c4220
Cooper Kelley <cooperkelley@Coopers-MacBook-Pro.local>
2019-01-08 16:53:42 -0700

    google drive access

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Axios from 'axios';
 
 class App extends Component {
+
+  state = {
+    movies: []
+  }
+
+  componentDidMount(){
+    Axios.get('/files').then(res => {
+      console.log(res.data)
+      this.setState({
+        movies: res.data
+      })
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      this.state.movies.map(movie => {
+        return (
+          <div>
+            <iframe 
+            src={`https://drive.google.com/file/d/${movie.id}/preview`} 
+            width="640" 
+            height="480"
+            allowfullscreen="allowfullscreen"
+            mozallowfullscreen="mozallowfullscreen" 
+            msallowfullscreen="msallowfullscreen" 
+            oallowfullscreen="oallowfullscreen" 
+            webkitallowfullscreen="webkitallowfullscreen"
+            ></iframe>
+          </div>
+        )
+      })
     );
   }
 }
