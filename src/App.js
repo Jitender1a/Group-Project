dab7d5c5b38efcdd53344809b2b7d4bf232bd938
Brendan Schiller <schiller.brendan@gmail.com>
2019-01-09 16:48:35 -0700

    end of day push

--- src/App.js
+++ src/App.js
@@ -12,6 +12,17 @@ import Nav from './components/Layout/Nav'
 import OurTeam from './components/Layout/OurTeam'
 import SideNav from './components/Layout/SideNav'
 import SplashScreen from './components/Layout/SplashScreen'
+import AllMovies from './components/Movies/AllMovies'
+import KidsMovies from './components/Movies/KidsMovies'
+import MovieGenres from './components/Movies/MovieGenres'
+import MoviesInfo from './components/Movies/MoviesInfo'
+import MovieTrailers from './components/Movies/MovieTrailers'
+import PopularMovies from './components/Movies/PopularMovies'
+import RandomMovies from './components/Movies/RandomMovies'
+import RecentlyWatchedMovies from './components/Movies/RecentlyWatchedMovies'
+import TrendingMovies from './components/Movies/TrendingMovies'
+
+
 
 
 class App extends Component {
@@ -26,6 +37,22 @@ class App extends Component {
           <Route path='/Register' component={Register}/>
           <Route path='/LoginHome' component={LoginHome}/>
           <Route path='/Logout' component={Logout}/>
+          <Route path='/AboutUs' component={AboutUs}/>
+          <Route path='/Contact' component={Contact}/>
+          <Route path='/Homepage' component={Homepage}/>
+          <Route path='/Nav' component={Nav}/>
+          <Route path='/OurTeam' component={OurTeam}/>
+          <Route path='/SideNav' component={SideNav}/>
+          <Route path='/SplashScreen' component={SplashScreen}/>
+          <Route path='/AllMovies' component={AllMovies}/>
+          <Route path='/KidsMovies' component={KidsMovies}/>
+          <Route path='/MovieGenres' component={MovieGenres}/>
+          <Route path='/MoviesInfo' component={MoviesInfo}/>
+          <Route path='/MovieTrailers' component={MovieTrailers}/>
+          <Route path='/PopularMovies' component={PopularMovies}/>
+          <Route path='/RandomMovies' component={RandomMovies}/>
+          <Route path='/RecentlyWatchedMovies' component={RecentlyWatchedMovies}/>
+          <Route path='/TrendingMovies' component={TrendingMovies}/>
        </Switch>
 
      </div>
--- src/components/Movies/AllMovies.js
+++ src/components/Movies/AllMovies.js
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { Component } from 'react'
+import {Link} from 'react-router-dom'
 
-export default function AllMovies() {
-  return (
-    <div>
-      AllMovies
-    </div>
-  )
-}
+export default class AllMovies extends Component {
+    render() {
+      return (
+        <div>
+          AllMovies
+        </div>
+      )
+    }
+  }
+  
