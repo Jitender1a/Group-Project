f2edd93a7b07339c07697639065fa82955e9792c
Brendan Schiller <schiller.brendan@gmail.com>
2019-01-23 09:46:41 -0700

    merge ready

--- src/components/Movies/DriveMovies/DriveMovieFunctions.test.js
+++ src/components/Movies/DriveMovies/DriveMovieFunctions.test.js
@@ -1,7 +1,28 @@
 const functions = require('./DriveMovieFunctions')
 
-describe('Testing the function movieTitles', () => {
+describe('Testing the function movieYear', () => {
+    test('Years should be an array', () => {
+        expect(Array.isArray(functions.movieYear("Jason Bourne(2016).mp4"))).toEqual(true)
+    })
     
+    test('Years should have 1 item in the array', () => {
+        expect(functions.movieYear("Jason Bourne(2016).mp4").length).toBe(1)
+    })
+    
+    test('Item in the years array should be a string', () => {
+        expect(typeof functions.movieYear("Jason Bourne(2016).mp4")[0]).toBe('string')
+    })
+    
+    test('Return value of movieYear should be 2016', () => {
+        expect(functions.movieYear("Jason Bourne(2016).mp4")[0]).toBe('2016')
+    })
+    
+    test('Return value of movieYear should only contain numbers', () => {
+        expect(functions.movieYear("Jason Bourne(2016).mp4")[0]).toMatch(/[0-9]/)
+    })
+})
+
+describe('Testing the function movieTitles', () => {
     test('Return value of movieTitles should only contain numbers', () => {
         expect(functions.movieTitles("Baby Driver (2017).mp4")[0]).toMatch(/[0-9]/)
     })
